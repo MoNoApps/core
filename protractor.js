e86f47754c52e3684007dfaae2213507f44513e4
Daniel Martinez <dmartz86@gmail.com>
2015-08-21 14:48:07 -0500

    Chrome browser test only

--- protractor.js
+++ protractor.js
@@ -10,8 +10,6 @@ exports.config = {
     './test/e2e/logout.test.js'
   ],
   multiCapabilities: [
-    { browserName: 'ie' },
-    { browserName: 'firefox' },
     { browserName: 'chrome'}
   ]
 };
