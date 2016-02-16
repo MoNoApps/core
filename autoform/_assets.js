02c1c6946b2060d91542ff0d257b722181cd2009
Daniel Martinez <dmartz86@gmail.com>
2016-02-16 18:00:03 +0000

    Add directives to gulp pipe.

--- autoform/_assets.js
+++ autoform/_assets.js
@@ -8,6 +8,7 @@ module.exports = function(cb) {
   ng.push('assets/javascript/app.js');
   ng.push('assets/javascript/services/*.js');
   ng.push('assets/javascript/controllers/*.js');
+  ng.push('assets/javascript/directives/*.js');
   js.push('./helpers/*.js');
   js.push('./api/*.js');
   js.push('./web/*.js');
@@ -16,6 +17,7 @@ module.exports = function(cb) {
   js.push('assets/javascript/*.js');
   js.push('assets/javascript/services/*.js');
   js.push('assets/javascript/controllers/*.js');
+  js.push('assets/javascript/directives/*.js');
   js.push('test/*.js');
   js.push('test/e2e/*.js');
   js.push('test/helpers/*.js');
@@ -32,11 +34,13 @@ module.exports = function(cb) {
       js.push('plugins/' + name + '/assets/javascript/*.js');
       js.push('plugins/' + name + '/assets/javascript/services/*.js');
       js.push('plugins/' + name + '/assets/javascript/controllers/*.js');
+      js.push('plugins/' + name + '/assets/javascript/directives/*.js');
       js.push('plugins/' + name + '/test/*.js');
       js.push('plugins/' + name + '/test/e2e/*.js');
       js.push('plugins/' + name + '/test/helpers/*.js');
       ng.push('plugins/' + name + '/assets/javascript/services/*.js');
       ng.push('plugins/' + name + '/assets/javascript/controllers/*.js');
+      ng.push('plugins/' + name + '/assets/javascript/directives/*.js');
       st.push('plugins/' + name + '/assets/styles/*.css');
     }
   }
