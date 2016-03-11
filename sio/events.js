5e29597f791f3237c38e1ff86fc934151de5f499
Daniel Martinez <dmartz86@gmail.com>
2016-03-11 00:36:26 +0000

    #29 Refactor sio

--- sio/events.js
+++ sio/events.js
@@ -1,24 +1,16 @@
 var db = require('../helpers/models');
 var review = require('../helpers/manager').review;
 
-var users = function(socket, cb) {
+function users(socket, cb) {
   review(
-    { req:
-      {params: {token: socket.token} },
-      res: {}
-    }, function(err, opt) {
-      db.users.Find({}, function(err, rsp){
-        cb(err, rsp);
-      }
-    );
-  });
-};
+    {req: {params: {token: socket.token} }, res: {} }, 
+    ((err, opt) => db.users.Find({}, ((err, rsp) =>  cb(err, rsp))))
+  );
+}
 
-var roles = function(socket, cb) {
-  db.roles.Find({}, function(err, rsp){
-    cb(err, rsp);
-  });
-};
+function roles(socket, cb) {
+  db.roles.Find({}, ((err, rsp) => cb(err, rsp)));
+}
 
 module.exports.users = users;
 module.exports.roles = roles;
--- sio/routes.js
+++ sio/routes.js
@@ -1,36 +1,56 @@
-var ps = require('../helpers/ps');
+var pub = require('../helpers/ps').pub;
 var sio = require('../web/routes').sio;
 var conf = require('../config.json');
 var events = require('./events');
 
-var listen = function(cb) {
-  var users = 0;
-  sio.on('connection', function (socket) {
-    ps.pub.set(conf.site + '::users', users += 1);
-    socket.emit('current', {users: users});
+function current() {
+  var count = pub.get(conf.site + '::users');
+  return {users: count};
+}
 
-    socket.on('identify', function (token) {
+function incUsers() {
+  pub.incr(conf.site + '::users');
+}
+
+function resetUsersCount() {
+  pub.set(conf.site + '::users', 0);
+}
+
+function disconnect() {
+  pub.decr(conf.site + '::users');
+}
+
+function listen(cb) {
+
+  resetUsersCount();
+
+  function connection(socket) {
+
+    incUsers();
+
+    function identify(token) {
       socket.token = token;
-    });
+    }
 
-    socket.on('getUsers', function() {
-      events.users(socket, function(err, users) {
-        socket.emit('users', users);
-      });
-    });
+    function getUsers() {
+      events.users(socket, ((err, users) => socket.emit('users', users)));
+    }
 
-    socket.on('getRoles', function() {
-      events.roles(socket, function(err, roles){
-        socket.emit('roles', roles);
-      });
-    });
+    function getRoles() {
+      events.roles(socket, ((err, roles) => socket.emit('roles', roles)));
+    }
 
-    socket.on('disconnect', function () {
-      ps.pub.set(conf.site + '::users', users -= 1);
-    });
-  });
+    socket.on('getRoles', getRoles);
+    socket.on('getUsers', getUsers);
+    socket.on('identify', identify);
+    socket.on('disconnect', disconnect);
+    socket.emit('current', current);
+
+  }
+
+  sio.on('connection', connection);
 
   cb();
-};
+}
 
 module.exports.listen = listen;
